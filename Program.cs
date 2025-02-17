9c1e8a04bac86078397f8b0cc9c0229605176d9a
Anaf02 <ana.florescu.02@gmail.com>
2025-02-17 20:36:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace coffee_shop_finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
