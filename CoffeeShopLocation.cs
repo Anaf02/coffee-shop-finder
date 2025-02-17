b20014953074daf1ae89dcd9ed63c778567acc4d
Anaf02 <ana.florescu.02@gmail.com>
2025-02-17 21:39:01 +0200

    add business logic - calculate distance and top three coffee shops

---
+++ CoffeeShopLocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffee_shop_finder
+{
+    public class CoffeeShopLocation : Location
+    {
+        public string Name { get; private set; }
+
+        public CoffeeShopLocation(double xCoordinate, double yCoordinate, string name) : base(xCoordinate, yCoordinate)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffee_shop_finder
+{
+    public class Location
+    {
+        public double XCoordinate { get; set; }
+
+        public double YCoordinate { get; set; }
+
+        public Location(double xCoordinate, double yCoordinate)
+        {
+            this.XCoordinate = xCoordinate;
+            this.YCoordinate = yCoordinate;
+        }
+    }
+}
